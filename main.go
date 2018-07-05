e926304ced2fcce6bbf304fec1177717e47c1401
Cajus Pollmeier <pollmeier@gonicus.de>
2018-07-05 11:53:30 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gogo/protobuf/jsonpb"
+    "github.com/cajus/nixgeht/proto"
+    "strings"
+)
+
+func main() {
+    m := jsonpb.Marshaler{}
+    str, err := m.MarshalToString(&pb.ParameterSets{Sets: map[string]*pb.ParameterSet{
+        "parameter": {
+            Raw: []byte{0, 1, 2, 3, 4, 5, 6, 1, 2},
+            Parameters: []*pb.Parameter{
+                {Name: "itest", Value: &pb.Parameter_IntValue{42}},
+                {Name: "stest", Value: &pb.Parameter_StrValue{"Foobar"}},
+                {Name: "btest", Value: &pb.Parameter_BoolValue{true}},
+                {Name: "ftest", Value: &pb.Parameter_FloatValue{41.99}},
+           },
+        },
+    }},)
+    fmt.Println(str)
+    fmt.Println(err)
+
+    sets := pb.ParameterSets{}
+    err = jsonpb.Unmarshal(strings.NewReader(str), &sets)
+    fmt.Println(sets)
+    fmt.Println(err)
+}
